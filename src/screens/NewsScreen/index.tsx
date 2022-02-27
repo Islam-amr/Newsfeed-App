2a138f71687cb6c094d82d278d092478d126592f
Eslam Amr <eslam.amr@beynd-solution.com>
2022-02-27 03:18:32 +0200

    feat: implement loading UI

--- src/screens/NewsScreen/index.tsx
+++ src/screens/NewsScreen/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { View, FlatList } from 'react-native'
+import { View, FlatList, Dimensions } from 'react-native'
+import SkeletonPlaceholder from "react-native-skeleton-placeholder";
 import axios from 'axios'
 
 // components import
@@ -11,17 +12,26 @@ import styles from './styles'
 // apis list import
 import apis from '../../utils/apis'
 
+const { height } = Dimensions.get('window')
+const skeltonItemsEstimation: number = Math.round(height * 0.9 / 160) - 1 // to estimate how much skeleton item could fit the phone screen
+const skeltonList: number[] = Array(skeltonItemsEstimation).fill(null).map((_, i) => i); // initialize sequenced array of the estimated length
+
+
 const renderNewsItem = ({ item }) => <NewsItem item={item} /> // to avoid anonymous function and re-render on renderItem
 
 const NewsScreen = () => {
     const [newsData, setNewsData] = useState(null) // to store news data 
+    const [loading, setLoading] = useState(false) // to handle waiting while api respond
 
     // to handle news api call
     const fetchNews = async () => {
+        setLoading(true)
         try {
             const response = await axios.get(apis.FETCH_NEWS)
             setNewsData(response.data.articles) // to update the news data
+            setLoading(false)
         } catch (e) {
+            setLoading(false)
         }
     }
 
@@ -30,13 +40,31 @@ const NewsScreen = () => {
         fetchNews()
     }, [])
 
+    if (loading) {
+        return (
+            <View style={styles.screenContainer}>
+                <View style={styles.marginContainer}>
+                    <SkeletonPlaceholder>
+                        {skeltonList.map((item: number) => {
+                            return (
+                                <SkeletonPlaceholder.Item key={item} alignItems="center" justifyContent='space-between' marginBottom={18} >
+                                    <View style={styles.skeletonCard} />
+                                </SkeletonPlaceholder.Item>
+                            )
+                        })}
+                    </SkeletonPlaceholder>
+                </View>
+            </View>
+        )
+    }
+
     return (
         <View style={styles.screenContainer}>
             <FlatList
                 data={newsData}
                 showsVerticalScrollIndicator={false}
-                style={styles.newslistContainer}
-                keyExtractor={(_, i) => i.toString()}
+                style={styles.marginContainer}
+                keyExtractor={(_, index) => index.toString()}
                 renderItem={renderNewsItem}
             />
         </View>
--- src/screens/NewsScreen/styles.ts
+++ src/screens/NewsScreen/styles.ts
@@ -1,12 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+import colors from '../../utils/colors';
+
+const { width } = Dimensions.get('window')
 
 export default StyleSheet.create({
     screenContainer: {
         flex: 1,
-        backgroundColor: 'black',
+        backgroundColor: colors.BLACK,
         paddingHorizontal: 14
     },
-    newslistContainer: {
+    marginContainer: {
         marginVertical: 14
+    },
+    skeletonCard: {
+        opacity: 0.2,
+        height: 160,
+        width: width - width * 0.07,
+        borderRadius: 14
     }
 });
